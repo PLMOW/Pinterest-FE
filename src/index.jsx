d3651dbc415a7bf366c8f45adf104a2031dc8dce
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-03 16:35:30 +0900

    fix: Redux-Provider 위치 변경

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Providers from 'libs/providers/Providers';
+import store from 'redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    <Provider store={store}>
+      <Providers>
+        <App />
+      </Providers>
+    </Provider>
   </React.StrictMode>
 );
--- src/libs/providers/Providers.jsx
+++ src/libs/providers/Providers.jsx
@@ -1,17 +1,15 @@
-import store from 'redux/store';
 import GlobalStyle from 'libs/style/Globalstyle';
-import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from 'theme';
+import { useSelector } from 'react-redux';
 
 const Providers = ({ children }) => {
+  const isDark = useSelector((state) => state.themeSlicer);
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        {children}
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
   );
 };
 
