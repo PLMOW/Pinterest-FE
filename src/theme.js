e3f614d88aab24e0dceaee4a4768307f522e4b79
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-03 15:58:30 +0900

    feat: darkTheme / lightTheme 추가

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,17 @@
-export const theme = {
-  color: '#2b2b2b',
+export const darkTheme = {
+  background: '#2b2b2b',
+  transparentBackground: 'rgba(43, 43, 43, 0.65)',
+  color: '#F5F6F7',
+  transparentColor: 'rgba(245, 246, 247, 0.65)',
+  pointColor: '#E60023',
+  transitionOption: 'ease-in-out 0.15s',
+};
+
+export const lightTheme = {
   background: '#F5F6F7',
-  transparentColor: 'rgba(43, 43, 43, 0.65)',
   transparentBackground: 'rgba(245, 246, 247, 0.65)',
+  color: '#2b2b2b',
+  transparentColor: 'rgba(43, 43, 43, 0.65)',
+  pointColor: '#E60023',
   transitionOption: 'ease-in-out 0.15s',
 };
