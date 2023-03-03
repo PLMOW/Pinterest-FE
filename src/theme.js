5e4ce166440bcbf0d5e41b973f4eb35a402cd1cc
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-03 15:32:23 +0900

    feat: GlobalTheme 추가

---
+++ src/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+  color: '#2b2b2b',
+  background: '#F5F6F7',
+  transparentColor: 'rgba(43, 43, 43, 0.65)',
+  transparentBackground: 'rgba(245, 246, 247, 0.65)',
+  transitionOption: 'ease-in-out 0.15s',
+};
