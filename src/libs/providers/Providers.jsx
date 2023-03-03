9ad7cefa46efa1d9dc23f2e55af3060fcd80056a
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-03 15:33:00 +0900

    feat: GlobalStyle theme DI

--- src/libs/providers/Providers.jsx
+++ src/libs/providers/Providers.jsx
@@ -1,12 +1,16 @@
 import store from 'redux/store';
 import GlobalStyle from 'libs/style/Globalstyle';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme';
 
 const Providers = ({ children }) => {
   return (
     <Provider store={store}>
-      <GlobalStyle />
-      {children}
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
     </Provider>
   );
 };
