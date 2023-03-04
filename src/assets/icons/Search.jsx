b65159325ca3eea896852fc31487e59b0e2ac43f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-04 14:53:37 +0900

    feat: svg assets 추가

---
+++ src/assets/icons/Search.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const Search = () => {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+      width="24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </Svg>
+  );
+};
+
+export default Search;
+
+const Svg = styled.svg`
+  color: white;
+  position: absolute;
+  left: 15px;
+`;
