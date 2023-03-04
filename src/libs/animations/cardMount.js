100d55fb1d607188cd1d34aeb2e111d5d48feaec
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-04 22:26:23 +0900

    feat: Card Mount animation 추가

---
+++ src/libs/animations/cardMount.js
@@ -0,0 +1,21 @@
+import gsap from 'gsap';
+import GSAP from 'libs/constants/gsap';
+
+const cardMount = () => {
+  gsap.fromTo(
+    GSAP.CARD.CARD_GSAP_REF,
+    { autoAlpha: 0 },
+    {
+      autoAlpha: 1,
+      overwrite: true,
+      duration: 0.15,
+      stagger: {
+        each: 0,
+        from: 'random',
+        amount: 1,
+      },
+    }
+  );
+};
+
+export default cardMount;
