7ff204d0c3257d35607654cfbc74b758f50bda99
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-04 15:18:39 +0900

    feat: Nav 추가

--- src/libs/providers/Providers.jsx
+++ src/libs/providers/Providers.jsx
@@ -2,12 +2,14 @@ import GlobalStyle from 'libs/style/Globalstyle';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from 'theme';
 import { useSelector } from 'react-redux';
+import Nav from 'shared/Nav';
 
 const Providers = ({ children }) => {
   const isDark = useSelector((state) => state.themeSlicer);
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
       <GlobalStyle />
+      <Nav />
       {children}
     </ThemeProvider>
   );
