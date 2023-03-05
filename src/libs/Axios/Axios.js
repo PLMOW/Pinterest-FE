cfc6ba847ad0cfdf0dde69c1da1c422da6efc9d5
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-06 01:51:43 +0900

    feat: Axios 객체 Interceptor 미들웨어 셋팅

--- src/libs/Axios/Axios.js
+++ src/libs/Axios/Axios.js
@@ -8,29 +8,79 @@ class Axios {
   #cookie;
 
   /**
-   * @param {boolean} auth
+   * @param {boolean} isAuthReq
    */
-  constructor(auth = false) {
+  constructor(isAuthReq = false) {
     this.#instance = axios.create({
       baseURL: `${process.env.REACT_APP_API_BASE_ROUTE}`,
     });
-    this.#auth = auth;
+    this.#auth = isAuthReq;
     this.#cookie = new Cookies();
-    if (auth) this.#setAuth(auth);
+    this.#setInterceptor();
   }
 
-  #setAuth(auth) {
-    this.#instance.interceptors.request.use(this.#reqAuthMiddleWare);
+  #setInterceptor() {
+    this.#instance.interceptors.request.use(
+      this.#reqMiddleWare.bind(this),
+      this.#reqOnError.bind(this)
+    );
+    this.#instance.interceptors.response.use(
+      this.#resMiddleWare.bind(this),
+      this.#resOnError.bind(this)
+    );
   }
 
-  #reqAuthMiddleWare = (config) => {
-    return {
-      ...config,
+  #reqMiddleWare(config) {
+    let newConfig = config;
+    if (this.#auth) newConfig = this.#setAuthReq(newConfig);
+
+    return newConfig;
+  }
+
+  #setAuthReq(config) {
+    const { headers } = config;
+    const newConfig = {
       headers: {
+        ...headers,
         Authorization: `${this.#cookie.get(COOKIE.ACCESS_TOKEN)}`,
       },
     };
-  };
+
+    return newConfig;
+  }
+
+  #reqOnError(error) {}
+
+  #resMiddleWare(res) {
+    this.#instance.interceptors.response.use();
+    const { authorization } = res.headers;
+    const { refreshtoken } = res.headers;
+
+    //if (authorization) //쿠키 넣어주기
+  }
+
+  #resOnError(error) {}
+
+  /**
+   * @param {string} endPoint
+   */
+  get(endPoint) {
+    this.#instance({
+      url: endPoint,
+    });
+  }
+
+  /**
+   * @param {string} endPoint
+   * @param {string} query
+   */
+  getByQuery(endPoint, query) {}
+
+  /**
+   * @param {string} endPoint
+   * @param {string} query
+   */
+  getByParams(endPoint, params) {}
 }
 
 export default Axios;
