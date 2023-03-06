32ca47dfa73b4e97e7ddb2e915a5f989bdae34e4
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-06 21:26:40 +0900

    feat: BoxDataSlicer 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import themeSlicer from './modules/themeSlicer';
 import searchSlicer from './modules/searchSlicer';
 import overlaySlicer from './modules/overlayToggle';
+import boxDataSlicer from './modules/boxDataSlicer';
 
 const store = configureStore({
-  reducer: { themeSlicer, searchSlicer, overlaySlicer },
+  reducer: { themeSlicer, searchSlicer, overlaySlicer, boxDataSlicer },
 });
 
 export default store;
