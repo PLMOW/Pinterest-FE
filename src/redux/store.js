93954676adf9063482862f1a0e40dbc5b9b3e00c
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-06 17:52:52 +0900

    feat: overlay store 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeSlicer from './modules/themeSlicer';
 import searchSlicer from './modules/searchSlicer';
+import overlaySlicer from './modules/overlayToggle';
 
 const store = configureStore({
-  reducer: { themeSlicer, searchSlicer },
+  reducer: { themeSlicer, searchSlicer, overlaySlicer },
 });
 
 export default store;
