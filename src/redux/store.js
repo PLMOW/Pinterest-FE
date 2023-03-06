3bc24898403adc85c81f92ab84d8a02471d29599
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-06 11:44:51 +0900

    feat: searchSlicer store 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeSlicer from './modules/themeSlicer';
+import searchSlicer from './modules/searchSlicer';
 
 const store = configureStore({
-  reducer: { themeSlicer },
+  reducer: { themeSlicer, searchSlicer },
 });
 
 export default store;
