e5392ca275f2b2aee4164e8b8facbd4cd9cf825d
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-07 18:57:32 +0900

    refactor: 하드코딩 된 값 변경

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,6 +1,6 @@
 const Test = () => {
   const kakaoLogin = () => {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=5d8ed25e63b7f00ff427d9b51608c030&redirect_uri=http://localhost:3009/oauth/kakao&response_type=code`;
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_API_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_API_BASE_ROUTE}oauth/kakao&response_type=code`;
   };
 
   return <button onClick={kakaoLogin}>카카오 로그인 버튼</button>;
