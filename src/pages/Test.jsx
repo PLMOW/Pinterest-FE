c339082e594044b1cf7e94b6ac17004555e15ae7
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-07 19:07:34 +0900

    feat: SocialLogin 컴포넌트 추가

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,9 +1,36 @@
+import styled from 'styled-components';
+
 const Test = () => {
   const kakaoLogin = () => {
+    console.log('click!');
     window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_API_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_API_BASE_ROUTE}oauth/kakao&response_type=code`;
   };
 
-  return <button onClick={kakaoLogin}>카카오 로그인 버튼</button>;
+  return (
+    <Wrapper>
+      <Button onClick={kakaoLogin}>카카오 로그인 버튼</Button>
+    </Wrapper>
+  );
 };
 
 export default Test;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Button = styled.div`
+  padding: 10px;
+  border-radius: 5px;
+  background: ${({ theme }) => theme.color};
+  color: ${({ theme }) => theme.background};
+  margin-top: 20px;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
