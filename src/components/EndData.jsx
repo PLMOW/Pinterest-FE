3af5970965cf28fb1fa18e8f4c0bbd4c2e98614a
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-08 14:39:56 +0900

    feat: 무한스크롤 추가

--- src/components/EndData.jsx
+++ src/components/EndData.jsx
@@ -1,11 +1,30 @@
 import styled from 'styled-components';
 import { useInView } from 'react-intersection-observer';
+import Axios from 'libs/Axios';
+import { useEffect, useState, useMemo } from 'react';
+import { setSearchValue } from 'redux/modules/searchSlicer';
+import { useDispatch, useSelector } from 'react-redux';
 
 const EndData = () => {
   const { ref, inView } = useInView({
-    threshold: 0,
+    threshold: 1,
   });
+  const [index, setIndex] = useState(0);
 
+  const dispatch = useDispatch();
+  const pins = useSelector((state) => state.searchSlicer);
+  const api = useMemo(() => new Axios(), []);
+
+  const getData = async () => {
+    const { data } = await api.getByQuery('api/pins', { index });
+    dispatch(setSearchValue([...pins, ...data]));
+  };
+  useEffect(() => {
+    if (inView) {
+      getData();
+      setIndex((prev) => (prev += 1));
+    }
+  }, [inView]);
   console.log(inView);
 
   return <Wrapper ref={ref}>{inView.toString()}</Wrapper>;
