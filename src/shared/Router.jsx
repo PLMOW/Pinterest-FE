3e447007b1c5c88266be2c1c19391fddd76205b1
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-08 21:29:19 +0900

    chore: 불필요한 Ruote 삭제

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,8 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Pins from 'pages/Pins';
 import Home from 'pages/Home';
 import Load from 'pages/Load';
-import Login from 'pages/Login';
-import Signin from 'pages/Signin';
 
 const Router = () => {
   return (
@@ -11,8 +9,6 @@ const Router = () => {
       <Route path="/" element={<Home />} />
       <Route path="/pins" element={<Pins />} />
       <Route path="/upload" element={<Load />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signin" element={<Signin />} />
     </Routes>
   );
 };
