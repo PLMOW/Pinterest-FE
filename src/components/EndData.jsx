ba2e60f158300c9cde46d81c07354d37648f34f2
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-08 18:15:55 +0900

    refactor: 무한스크롤 커스텀훅 분리

--- src/components/EndData.jsx
+++ src/components/EndData.jsx
@@ -1,33 +1,10 @@
 import styled from 'styled-components';
-import { useInView } from 'react-intersection-observer';
-import Axios from 'libs/Axios';
-import { useEffect, useState, useMemo } from 'react';
-import { setSearchValue } from 'redux/modules/searchSlicer';
-import { useDispatch, useSelector } from 'react-redux';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
 
 const EndData = () => {
-  const { ref, inView } = useInView({
-    threshold: 1,
-  });
-  const [index, setIndex] = useState(0);
+  const [ref, isLoading] = useInfiniteScroll();
 
-  const dispatch = useDispatch();
-  const pins = useSelector((state) => state.searchSlicer);
-  const api = useMemo(() => new Axios(), []);
-
-  const getData = async () => {
-    const { data } = await api.getByQuery('api/pins', { index });
-    dispatch(setSearchValue([...pins, ...data]));
-  };
-  useEffect(() => {
-    if (inView) {
-      getData();
-      setIndex((prev) => (prev += 1));
-    }
-  }, [inView]);
-  console.log(inView);
-
-  return <Wrapper ref={ref}>{inView.toString()}</Wrapper>;
+  return <>{isLoading ? <>Loading</> : <Wrapper ref={ref}></Wrapper>}</>;
 };
 
 export default EndData;
